177a96d2972dcf3b8f0b31fd6093b2025928ec2f
Philip ks <3000piksel@gmail.com>
2024-05-27 19:33:59 +0200

    initial commit

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# cheat sheets
+miscellaneous cheat sheets and how to generate them
+> not a book the ordinary wizard would come across
+
+## Table of Contents
+> [!NOTE]
+> TBD.
---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs =
+          import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-basic
+            environ etoolbox geometry listings listingsutf8 pdfcol pgf tcolorbox tikzfill tools;
+        };
+
+      in
+      {
+        devShells.default = (pkgs.devshell.mkShell {
+          imports = [ "${devshell}/extra/git/hooks.nix" ];
+          name = "tex-shell";
+          packages = with pkgs; [ tex nixpkgs-fmt ];
+          git.hooks = {
+            enable = true;
+            pre-commit.text = ''
+              nix flake check
+            '';
+          };
+          commands = [
+            {
+              name = "make";
+              command = ''
+                pdflatex -output-directory . -interaction nonstopmode -halt-on-error -file-line-error -jobname $(basename $(pwd)) output.tex
+                rm -f *.aux *.log *.out
+              '';
+              help = "compile pdf";
+            }
+            {
+              name = "clean";
+              command = ''
+                rm -f *.aux *.log *.out
+              '';
+              help = "remove temporary files";
+            }
+            {
+              name = "prune";
+              command = ''
+                rm -f *.pdf *.aux *.log *.out
+              '';
+              help = "remove all output files";
+            }
+            {
+              name = "new";
+              command = ''
+                cp --update=none ../output.tex .
+                chown philip:philip *.tex
+              '';
+              help = "create new tex";
+            }
+          ];
+        });
+      }
+    );
+}
